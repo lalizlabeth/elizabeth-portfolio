fd67df58ba3c4f33c25f2be7986fbe73a4fb5b30
lalizlabeth <elizabethylin@gmail.com>
2024-08-04 21:30:35 -0400

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,42 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="/favicon.icon" />
         <title>Elizabeth Lin: Product Design Leader &amp; Educator</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="" />
+        <meta
+          name="twitter:title"
+          content="Elizabeth Lin: Product Design Leader & Educator"
+        />
+        <meta
+          name="twitter:description"
+          content="Building an alternative design school. Previously at Primer, Lambda School, & Khan Academy"
+        />{" "}
+        <meta
+          name="twitter:image"
+          content="https://elizabeth-portfolio-tau.vercel.app/og-image.png"
+        />
+        <meta name="twitter:site" content="@lalizlabeth"></meta>
+        <meta
+          name="description"
+          content="Building an alternative design school. Previously at Primer, Lambda School, & Khan Academy"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          property="og:title"
+          content="Elizabeth Lin: Product Design Leader & Educator"
+        />
+        <meta
+          property="og:image"
+          content="https://elizabeth-portfolio-tau.vercel.app/og-image@2x.png"
+        />
+        <meta
+          property="og:description"
+          content="Building an alternative design school. Previously at Primer, Lambda School, & Khan Academy"
+        />
+        <meta
+          name="keywords"
+          content="elizabeth lin, lalizlabeth, product design, product design manager, educator, leader, khan academy, lambda school, primer, mentorship"
+        />
       </Head>
       <body className={`${alegreya.variable} ${ibm_plex_mono.variable}`}>
         {children}
