cc8da006154b3f231a84a8d0e12c9ab82659dd46
elizabeth lin <elizabethylin@gmail.com>
2025-05-09 07:22:09 -0400

    fix

--- src/app/who-wore-what/page.tsx
+++ src/app/who-wore-what/page.tsx
@@ -1,1 +1,9 @@
-import styles from './who-wore-what.module.css' 
+import styles from './who-wore-what.module.css'
+
+export default function WhoWoreWhat() {
+  return (
+    <main className={styles.container}>
+      <h1>Who wore what</h1>
+    </main>
+  )
+} 
